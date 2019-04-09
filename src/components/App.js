f12d1d709a67c1d8caa57c87f10a092919258817
Van Tran <vantran@pursuit.org>
2019-04-09 19:27:56 -0400

    createpatch uploads

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,14 @@
 import React, { Component } from "react";
+import {Route} from 'react-router-dom';
 import "./App.css";
+
+import createPatch from '../components/createPatch';
 
 class App extends Component {
   render() {
-    return <div>App</div>;
+    return <div>
+      <Route path= "/createpatchtesting" exact component ={createPatch} />
+    </div>;
   }
 }
 
--- src/components/createPatch.js
+++ src/components/createPatch.js
@@ -1,34 +1,85 @@
 import React, { Component } from 'react';
 import { Box } from "gestalt";
 import axios from 'axios';
+import * as firebase from 'firebase';
+// import firebase from '../firebase';
+import ImageService from '../services/image';
 
 class createPatch extends Component {
-    state = {
-        selectedFile: null
+
+    state={
+        savedImgURL: '',
     }
 
-    fileSelectedHandler = event => {
-        this.setState({
-            selectedFile: event.target.files[0]
+
+    saveImage = (url) => {
+        const date = Date();
+    
+        ImageService.saveImage(url, date);
+      }
+    
+      handleFileInput = (e) => {
+          
+        // const firstFile = e.target.files[0];
+        console.log("here",  e.target.files[0]);
+        if(e.target.files.length){
+          console.log("there is a file")
+          if (e.target.files[0]['name']){
+            // DO YOUR FIREBASE LOGIC HERE
+            console.log(e.target.files[0]);
+            const firstFile = e.target.files[0];
+            const root = firebase.storage().ref()
+          const newImage = root.child(firstFile.name);
+    
+        newImage.put(firstFile)
+          .then((snapshot) => {
+            return snapshot.ref.getDownloadURL()
+          })
+          .then((url) => {
+            console.log('GIVE ME URL', url)
+            this.saveImage(url);
+            this.setState({
+                savedImgURL: url
+            })
+          })
+        .catch((err) => {
+          console.log(err);
         })
-    }
-
-    fileUploadHandler = () => {
-        axios.post('https://us-central1-patch-that.cloudfunctions.net/uploadFile');
-    }
-
-
-    render() {
+          }
+        }
+        // const root = firebase.storage().ref()
+        // const newImage = root.child(firstFile.name);
+    
+        // newImage.put(firstFile)
+        //   .then((snapshot) => {
+        //     return snapshot.ref.getDownloadURL()
+        //   })
+        //   .then((url) => {
+        //     console.log('GIVE ME URL', url)
+        //     this.saveImage(url);
+        //     this.setState({
+        //         savedImgURL: url
+        //     })
+        //   })
+        // .catch((err) => {
+        //   console.log(err);
+        // })
+        
+      }
+    
+      render() {
+    
         return (
-        <Box>
-            <div className="createPatch">
-            <input type="file" onChange={this.fileSelectedHandler}/>
-            <button onClick={this.fileUploadHandler}>Upload Artwork</button>
+          <div className='container'>
+            <div className="input-group mb-3">
+              <div className="custom-file">
+                <input type="file" className="custom-file-input" onClick={this.handleFileInput} />
+                <label className="custom-file-label">Upload Image</label>
+              </div>
             </div>
-        </Box>
-
-        )
+          </div>
+        );
+      }
     }
-}
 
 export default createPatch;
