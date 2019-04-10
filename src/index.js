eec2d258c6f0d7dd85f6f8c421ff76db3a8ff29c
Van Tran <vantran@pursuit.org>
2019-04-10 00:13:57 -0400

    added carousel

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import 'gestalt/dist/gestalt.css';
-import App from "./components/App";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from "./components/Navbar";
 import Signin from "./components/Signin";
 import Signup from "./components/Signup";
 import Checkout from "./components/Checkout";
 import CreatePatch from "./components/createPatch";
+import Home from "./components/home";
 
 import registerServiceWorker from "./registerServiceWorker";
 
@@ -16,21 +17,24 @@ const Root = () => (
     <Router>
         <React.Fragment>
             <Navbar />
-                <Route component={App} path="/" />
+            
+                <Route component={Home} exact path="/" />
                 <Route component={Signin} path="/signin" />
                 <Route component={Signup} path="/signup" />
                 <Route component={Checkout} path="/checkout" />
                 <Route component={CreatePatch} path="/createPatch" />
-                
+            
         </React.Fragment>
     </Router>
-)
+);
 
-ReactDOM.render(<Root />, document.getElementById('root'));
+ReactDOM.render(<Root />, document.getElementById("root"));
+registerServiceWorker();
 
 if (module.hot) {
-    module.hot.accept();
+  module.hot.accept();
 }
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
